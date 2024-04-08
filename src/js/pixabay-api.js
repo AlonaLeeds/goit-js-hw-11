2d81d4781b8b503c612117ad7077d631bee45142
HelenYakovenko <elenus.light@gmail.com>
2024-04-08 16:49:51 +0300

    js api

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,63 +1,55 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
 
-const API_KEY = "43218628-434600724cd02d3e35d2dc910";
-const API_URL = "https://pixabay.com/api/";
+import axios from 'axios';
 
-const searchForm = document.querySelector("#search-form");
-const searchInput = document.querySelector("#search-input");
-const gallery = document.querySelector("#gallery");
+export default class NewsApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.PER_PAGE = 40;
+  }
+  async fetchGallery() {
+    const axiosOptions = {
+      method: 'get',
+      url: 'https://pixabay.com/api/',
+      params: {
+        key: '43218628-434600724cd02d3e35d2dc910',
+        q: `${this.searchQuery}`,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: `${this.page}`,
+        per_page: `${this.PER_PAGE}`,
+      },
+    };
+    try {
+      const response = await axios(axiosOptions);
 
-searchForm.addEventListener("submit", handleSubmit);
+      const data = response.data;
 
-function handleSubmit(event) {
-  event.preventDefault();
-
-  const searchTerm = searchInput.value.trim();
-
-  if (!searchTerm) {
-    iziToast.error({
-      title: "Error",
-      message: "Please enter a search term",
-    });
-    return;
+      this.incrementPage();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  searchImages(searchTerm);
-}
+  incrementPage() {
+    this.page += 1;
+  }
 
-function searchImages(searchTerm) {
-  fetch(`${API_URL}?key=${API_KEY}&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Failed to fetch images");
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.info({
-          title: "Info",
-          message: "Sorry, there are no images matching your search query. Please try again!",
-        });
-        return;
-      }
-      displayImages(data.hits);
-    })
-    .catch(error => {
-      iziToast.error({
-        title: "Error",
-        message: error.message,
-      });
-    });
-}
+  resetPage() {
+    this.page = 1;
+  }
 
-function displayImages(images) {
-  gallery.innerHTML = "";
-  images.forEach(image => {
-    const img = document.createElement("img");
-    img.src = image.webformatURL;
-    img.alt = image.tags;
-    gallery.appendChild(img);
-  });
+  resetEndOfHits() {
+    this.endOfHits = false;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
